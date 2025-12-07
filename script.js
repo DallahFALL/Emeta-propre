976b03c62ea7e5f994fb30a00cf2749b02f094ca
DallahFALL <afall644@gmail.com>
2025-12-07 15:41:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
    ===================================================== */
 
 /* --- Webhook Make (fonctionnel) --- */
-const MAKE_WEBHOOK_URL = "https://hook.eu2.make.com/uglhhhhfgia0sbfgscbsg1akaeejxck4";
+const MAKE_WEBHOOK_URL = "https://hook.eu2.make.com/4o3lzar12nkps9d8oplwk6ge4m0br5vl";
 
 /* --- Config langues --- */
 const LANG_CONFIG = {
