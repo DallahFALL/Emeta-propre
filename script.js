3adac8059f4c0b55a37d85cfbe2009c01c9f8b8c
DallahFALL <afall644@gmail.com>
2026-06-04 00:45:35 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // CONFIGURATION GLOBALE
 // ==========================================
 // ⚠️ REMPLACEZ CETTE URL PAR VOTRE TEST URL n8n !
-const WEBHOOK_N8N_URL = "http://localhost:5678/webhook/matrice-auto-detection"; 
+const WEBHOOK_N8N_URL = "const WEBHOOK_N8N_URL = "https://automation.e-metalabs.com/webhook/matrice-auto-detection"; 
 
 // ==========================================
 // DICTIONNAIRE MULTILINGUE (UI)
